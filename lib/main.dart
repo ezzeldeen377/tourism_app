01ee7a56958a19f2deed0c418a04952925f0d0fa
ezzeldeen377 <ezzeldeentv20@gmail.com>
2025-03-18 20:26:14 +0200

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:new_flutter/core/notifications/fcm.dart';
 import 'package:new_flutter/core/utils/location_permission_handler.dart';
 import 'package:new_flutter/core/widgets/contants.dart';
 import 'package:new_flutter/features/Auth/presentation/pages/login/widgets/login.dart';
 import 'package:new_flutter/features/splash/presentation/splash_view.dart';
 import 'package:new_flutter/start_app/start_page.dart';
 import 'package:new_flutter/features/splash/presentation/widgets/splash_body.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
- await LocationPermissionHandler.checkAndRequestPermission();
+  await LocationPermissionHandler.checkAndRequestPermission();
   await LocationPermissionHandler.handleLocationPermission();
-    Gemini.init(apiKey: "AIzaSyCk6lnyB8-7Nw1QSODDaSKMVpe5_uEiVcI");
+  Gemini.init(apiKey: "AIzaSyCk6lnyB8-7Nw1QSODDaSKMVpe5_uEiVcI");
 
+
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+
+  OneSignal.initialize("860b853c-cb34-4685-b6b7-9dccd85c5fd7");
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+OneSignal.Notifications.addForegroundWillDisplayListener((event) {
+    event.preventDefault(); // Prevent OneSignal's default handling
+    OneSignal.Notifications.displayNotification(event.notification.notificationId); // Manually show notification
+  });
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.grey[50],
@@ -38,8 +51,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
- 
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
